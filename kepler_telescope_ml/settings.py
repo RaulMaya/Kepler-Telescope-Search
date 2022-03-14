12c77864b8483ae4130ad656b03d5c47671038b8
Raul <raulmayas20@gmail.com>
2022-03-14 12:58:26 -0600

    Setting up a standby view in Django project

--- kepler_telescope_ml/settings.py
+++ kepler_telescope_ml/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from config import secretkey_rm 
+from .config import secretkey_rm 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
